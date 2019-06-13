af7090ccc2768755b2ab5fc7d990f74cd7ea379f
yishibakaien <646932161@qq.com>
2019-06-13 23:20:30 +0800

    feat(examples): init examples express server.js

---
+++ examples/server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const webpack = require('webpack')
+const webpackDevMiddleware = require('webpack-dev-middleware')
+const webpackHotMiddleware = require('webpack-hot-middleware')
+const WebpackConfig = require('./webpack.config')
+
+const app = express()
+const complier = webpack(WebpackConfig)
+
+app.use(webpackDevMiddleware(complier, {
+  publicPath: '/__build__/',
+  stats: {
+    colors: true,
+    chunks: false
+  }
+}))
+
+app.use(webpackHotMiddleware(complier))
+
+app.use(express.static(__dirname))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const router = express.Router()
+
+router.get('/simple/get', function (req, res) {
+  res.json({
+    msg: 'hello world'
+  })
+})
+
+app.use(router)
+
+const port = process.env.PORT || 8080
+module.exports = app.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}, Ctrl+C to stop`)
+})
