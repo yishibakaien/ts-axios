6c649d87077d019f80f04c4fe475c2e83bc93278
yishibakaien <646932161@qq.com>
2019-06-13 23:43:59 +0800

    feat(examples): examples finished

---
+++ examples/simple/app.ts
@@ -0,0 +1,11 @@
+import axios from '../../src/index'
+
+axios({
+  method: 'get',
+  url: '/simple/get',
+  params: {
+    a: 1,
+    b: 2
+  }
+})
+
--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -4,7 +4,7 @@ const webpack = require('webpack')
 
 module.exports = {
   mode: 'development',
-  entry: readdirSync(__dirname).reduce((entries, dir) => {
+  entry: fs.readdirSync(__dirname).reduce((entries, dir) => {
     const fullDir = path.join(__dirname, dir)
     const entry = path.join(fullDir, 'app.ts')
     if (fs.statSync(fullDir).isDirectory() && fs.existsSync(entry)) {
