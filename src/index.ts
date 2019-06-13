8336aed77baa8b87f694ddc801f96745580e4179
yishibakaien <646932161@qq.com>
2019-06-14 00:20:05 +0800

    feat: process AxiosRequestConfig

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
+import { buildURL } from './helpers/url'
 
 function axios(config: AxiosRequestConfig): void {
+  precessConfig(config)
   xhr(config)
+}
+
+function precessConfig(config: AxiosRequestConfig): void {
+  config.url = transformURL(config)
+}
+
+function transformURL(config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return buildURL(url, params)
 }
 
 export default axios
