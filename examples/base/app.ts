c66ef7afd383c9b687326dd0fe401e6b3564b0c5
yishibakaien <646932161@qq.com>
2019-06-14 00:39:57 +0800

    feat: base example finished

---
+++ examples/base/app.ts
@@ -0,0 +1,63 @@
+import axios from '../../src/index'
+
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    foo: ['bar', 'baz']
+  }
+})
+
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    foo: {
+      bar: 'baz'
+    }
+  }
+})
+
+const date = new Date()
+
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    date
+  }
+})
+
+
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    foo: '@:$, '
+  }
+})
+
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    baz: 'bar',
+    foo: null,
+  }
+})
+
+axios({
+  method: 'get',
+  url: '/base/get#hash',
+  params: {
+    foo: 'baz'
+  }
+})
+
+axios({
+  method: 'get',
+  url: '/base/get?baz=foo',
+  params: {
+    foo: 'bar'
+  }
+})
--- examples/server.js
+++ examples/server.js
@@ -31,6 +31,10 @@ router.get('/simple/get', function (req, res) {
   })
 })
 
+router.get('/base/get', function (req, res) {
+  res.json(req.query)
+})
+
 app.use(router)
 
 const port = process.env.PORT || 8080
