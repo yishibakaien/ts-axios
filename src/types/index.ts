a1614ca3b774df0605aa0c35d558c135ed753a9b
yishibakaien <646932161@qq.com>
2019-06-14 14:58:37 +0800

    chore: remove unnecessary import

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,3 @@
-import { request } from "https";
-
 export type Method =
   | 'get'
   | 'GET'
@@ -22,7 +20,7 @@ export interface AxiosRequestConfig {
   data?: any
   params?: any
   headers?: any
-  responseType?: XMLHttpRequestResponseType 
+  responseType?: XMLHttpRequestResponseType
 }
 
 export interface AxiosResponse {
@@ -34,6 +32,4 @@ export interface AxiosResponse {
   request: any
 }
 
-export interface AxiosPromise extends Promise<AxiosResponse> {
-
-}
+export interface AxiosPromise extends Promise<AxiosResponse> {}
