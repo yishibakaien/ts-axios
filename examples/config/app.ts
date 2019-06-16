168915dc9ba15efb93beec9f50215e66c2ae3328
yishibakaien <646932161@qq.com>
2019-06-16 22:47:37 +0800

    feat: request and response configurable example done

--- examples/config/app.ts
+++ examples/config/app.ts
@@ -1,4 +1,4 @@
-import axios from '../../src'
+import axios, { AxiosTransformer } from '../../src'
 import qs from 'qs'
 
 axios.defaults.headers.common['test2'] = 123
@@ -9,6 +9,32 @@ axios({
   data: qs.stringify({ a: 1 }),
   headers: {
     test: '321'
+  }
+}).then(res => {
+  console.log(res.data)
+})
+
+// 请求和响应配置化 demo
+axios({
+  transformRequest: [
+    (function(data) {
+      return qs.stringify(data)
+    }),
+    ...(axios.defaults.transformRequest as AxiosTransformer[])
+  ],
+  transformResponse: [
+    ...(axios.defaults.transformResponse as AxiosTransformer[]),
+    function(data) {
+      if (typeof data === 'object') {
+        data.b = 'transform respone mark'
+      }
+      return data
+    }
+  ],
+  url: '/config/post',
+  method: 'post',
+  data: {
+    a: 1
   }
 }).then(res => {
   console.log(res.data)
