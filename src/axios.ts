6c43df7d5790ee603394c791dc00f73e26110dec
yishibakaien <646932161@qq.com>
2019-06-16 22:58:07 +0800

    feat: add axios.create interface to instantiate a new Axios instance

--- src/axios.ts
+++ src/axios.ts
@@ -1,17 +1,22 @@
-import { AxiosInstance, AxiosRequestConfig } from "./types";
+import { AxiosRequestConfig, AxiosStatic } from "./types";
 import Axios from "./core/Axios";
 import { extend } from "./helpers/util";
 import defaults from "./defaults";
+import mergeConfig from "./core/mergeConfig";
 
-function createInstance(config: AxiosRequestConfig): AxiosInstance {
+function createInstance(config: AxiosRequestConfig): AxiosStatic {
   const context = new Axios(config)
   const instance = Axios.prototype.request.bind(context)
 
   extend(instance, context)
 
-  return instance as AxiosInstance
+  return instance as AxiosStatic
 }
 
 const axios = createInstance(defaults)
+
+axios.create = function create(config) {
+  return createInstance(mergeConfig(defaults, config))
+}
 
 export default axios
--- src/types/index.ts
+++ src/types/index.ts
@@ -82,6 +82,10 @@ export interface AxiosInstance extends Axios{
   <T=any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
 }
 
+export interface AxiosStatic extends AxiosInstance {
+  create(config?: AxiosRequestConfig): AxiosInstance
+}
+
 export interface AxiosInterceptorManager<T> {
   // 返回值的 number 是这个 interceptor 的 ID 用于 eject 的时候删除此 interceptor
   use(resolved: ResolvedFn<T>, rejected?: RejectedFn): number
